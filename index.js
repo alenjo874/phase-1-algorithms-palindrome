6e72dd325f76a7cdf8801d4d6a7040f60c2b76b4
Alen Jo <alenjo874@gmail.com>
2022-01-21 15:03:46 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let wordSplit = word.split("");
+
+  let flippedWord = [];
+  wordSplit.map((word) => {
+    flippedWord.unshift(word);
+  });
+
+  if (word === flippedWord.join("")) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  string should equal forward and backwards
+  split string into array
+  use array method
+  compare
 */
 
 /*
   Add written explanation of your solution here
+  Use split method so the string turns into an array with each letter.
+  Now we can easily iterate over it with an array method
+  such as .map. The goal is to see if the string is the same
+  forwards and backwards. So create a new empty array and insert
+  them with unshift, so that it adds them from the start. Using 
+  methods such as .push, will add each letter to the end, which
+  only recreates the same word. Then join the new flipped string, 
+  and compare it with the orginal string to return true or false. 
+
 */
 
 // You can run `node index.js` to view these console logs
