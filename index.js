e2dd38358bcb78791c3c7a960c08326b561c2db0
Alen Jo <alenjo874@gmail.com>
2022-01-21 15:00:08 -0500

    commit tree 06997aa4cce92fc1b5a438b7c5b86501696af0cf

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let wordSplit = word.split("");
+
+  let flippedWord = [];
+  wordSplit.map((word) => {
+    flippedWord.unshift(word);
+  });
+
+  if (word === flippedWord.join("")) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  string should equal forward and backwards
+  split string into array
+  use array method
+  compare
 */
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
